91fa4d1968aab515c326d763424471363bb2f78b
Your Name <you@example.com>
2026-05-03 16:16:46 -0300

    teste 404

--- app/page.js
+++ app/page.js
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <h1>Site funcionando</h1>;
+  return (
+    <main style={{color: "white", background: "black", height: "100vh"}}>
+      <h1>FUNCIONOU 🚀</h1>
+    </main>
+  );
 }
